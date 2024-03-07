720251a7e644cae98adcdc851ffa63c5c5673d56
AnnaIllarionova <annailinas@gmail.com>
2024-03-07 14:34:40 +0300

    debug

--- src/components/repos-order-component/repos-order-component.jsx
+++ src/components/repos-order-component/repos-order-component.jsx
@@ -1,21 +1,31 @@
 import * as S from "./repos-order.styled"
 
-export const ReposOrderComponent = ({ setReposOrder, setChosenButton, chosenButton }) => {
-   
-
+export const ReposOrderComponent = ({
+  setReposOrder,
+  setChosenButton,
+  chosenButton,
+}) => {
   return (
     <S.OrderDiv>
       <S.OrderTitle>Сортировать по количеству репозиториев:</S.OrderTitle>
-      <S.OrderButton onClick={() => {
-        setReposOrder("desc")
-        setChosenButton("по убыванию")
-        }}  
-        chosen={chosenButton === "по убыванию"}>по убыванию</S.OrderButton>
-      <S.OrderButton onClick={() => {
-        setReposOrder("asc")
-        setChosenButton("по возрастанию")
+      <S.OrderButton
+        onClick={() => {
+          setReposOrder("desc")
+          setChosenButton("по убыванию")
         }}
-        chosen={chosenButton === "по возрастанию"}>по возрастанию</S.OrderButton>
+        chosen={chosenButton === "по убыванию"}
+      >
+        по убыванию
+      </S.OrderButton>
+      <S.OrderButton
+        onClick={() => {
+          setReposOrder("asc")
+          setChosenButton("по возрастанию")
+        }}
+        chosen={chosenButton === "по возрастанию"}
+      >
+        по возрастанию
+      </S.OrderButton>
     </S.OrderDiv>
   )
 }
